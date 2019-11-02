979483efb6fd1e121624de48c933a0402706c95b
Francesco <francescoguaianav8@outlook.it>
2019-11-02 19:59:48 +0100

    Entsoe Web Crawler Primi tentativi con requests e lxml, login session. ____ Ho scaricato un po' di dati da questo www.entsoe.eu. Ho riassunto alcune features dello scraper e dell'analisi delle pagine del sito in questione in un documento (Word al momento). Update by Ciccio 2 Novembre.
    Il mio task per il 22 di Novembre è lavorare in parallelo con Luca nella realizzazione
    dei web crawler + Fase di ricerca e studio sugli articoli che ho arbitrariamente selezionato.
    Parte 1: WEB CRAWLER:
    
    Luca ha deciso di annichilire la GME.
    A me è toccato il sito entsoe.eu.
    Ho preparato un doc che sto allegando qui e caricando nel portale
    per descrivere il problema passo dopo passo e le info più importanti sul sito in questione.
    Sto procedendo a pushare il branch. Stato delle cose:
    ho implementato due sessioni per il login- login.py  e login2.py
    Ho provato ad analizzare le richieste GET della suddetta sessione
    al link:
    link primario: https://transparency.entsoe.eu/
    link login: https://transparency.entsoe.eu/homepageLogin?url=%2Fdashboard%2Fshow&username=s267534%40studenti.polito.it&password=SmartGridProject!
    come potete vedere bisogna fare attenzione alla codifica %40 della @
    e ai parametri che vengono passati da quella che sull'HTML viene definita una richiesta POST, ma che sull'ispezione
    dell'elemento al SIGN IN /action --> viene descritta come funzione get per accedere al link:
    
    https://transparency.entsoe.eu/dashboard/show
    
    una volta entrati_
    ho cominciato a osservare i file downloadabili.
    notando le feature discriminanti per avviare il download giusto.
    
    Purtroppo in questi ultimi 3 giorni, non ci ho messo mano. ho lavorato ad altre cose
    ( video editing) e raspberry per IoT.
    Ho tante cose da sistemare nel codice, tantissime!
    ma la mia prossima settimana sarà dedicata a quello e spero nell'aiuto di luca
    per poter risolvere qualche problema dovuto alla mia scarna abilità di programmazione web.
    
    Detto questo, questa sera c'è la Juve.. fatemela godere
    e domani rimetto mani sul codice!
    P.S: Scrivete qui per qualche altra info... .
    Per le chiamate famo domani.
    
    PARTE 2:  Research
    
    Testi selezionati:
    1.(Bompard) Bidding strategy with forecast technology based on support vector machine in the electricity market
    2.Competitive Strategic Bidding Optimization inElectricity Markets Using Bilevel Programming and Swarm Technique
    3. Strategic bidding in vertically integrated power markets with an application to the Italian electricity auctions

--- src/spiders/entsoe.py
+++ src/spiders/entsoe.py
@@ -1,6 +1,10 @@
 import os
 import time
 import requests as req
+from selenium import webdriver
 
 class EntsoeSpider():
     def __init__(self, log):
+        # Class init
+        self.driver = webdriver.Firefox(profile)
+        self.log = log
