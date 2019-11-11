eb354a0c5702d39d656486367fdeb5a9f1c64ca7
1uc4 <lucagioack@gmail.com>
2019-11-11 02:32:17 +0100

    Menu button pressed. TODO graph click

--- src/spiders/terna.py
+++ src/spiders/terna.py
@@ -1,36 +1,50 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+
 import logging
 import logging.config
+import requests as req
 
 # logging.config.fileConfig('src/logging.conf')
 # logger = logging.getLogger(__name__)
 
 class TernaSpider():
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+        self.driver.set_page_load_timeout(10)
 
-    def setUp(self):
-        driver= webdriver.Chrome()
-        driver.set_page_load_timeout(10)
-        connection = driver.get("https://www.terna.it/it/sistema-elettrico/transparency-report/actual-generation")
-        time.sleep(10)
-        driver.maximize_window()
-        return connection
+    def clickGraph(self):
+        self.driver.get("https://www.terna.it/it/sistema-elettrico/transparency-report/actual-generation")
+        self.driver.switch_to.frame(self.driver.find_element_by_id("iframeActualGen"))
 
-
-    def setElemets(self, connection):
-        _input=connection.find_elements_by_xpath("/html/body/div[2]/ui-view/div/div[1]/div/div/div/div/exploration-container/exploration-container-legacy/div/div/exploration-host/div/div/exploration/div/explore-canvas-modern/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container-modern[28]/transform/div/visual-container-header-modern/div/div[1]/div/visual-container-options-menu/visual-header-item-container/div/button/i")
-        time.sleep(4)
-        print("Test completato")
-
-
-
-def main():
-    spider = TernaSpider()
-    url=spider.setUp()
-    spider.setElemets(url)
-
+        try:
+            myElem = wait(self.driver, 10).until(ec.frame_to_be_available_and_switch_to_it((By.XPATH, '/html/body/div/iframe')))
+        except TimeoutError:
+            time.sleep(2)
+            
+        got = False
+        while not got:
+            try:       
+                btn = self.driver.find_element_by_class_name('vcMenuBtn')
+                self.driver.execute_script('arguments[0].click();', btn)
+                btn = self.driver.find_element_by_xpath("/html/body/div[9]/drop-down-list/ng-transclude/ng-repeat[1]/drop-down-list-item/ng-transclude/ng-switch/div")
+                self.driver.execute_script('arguments[0].click();', btn)
+                got=True
+            except TimeoutException:
+                print('Timeout')
+                time.sleep(1)
+            except NoSuchElementException:
+                print('Try Again')
+                time.sleep(2)
 
 if __name__ == '__main__':
-    main()
-
+    spider = TernaSpider()
+    spider.clickGraph()
 
